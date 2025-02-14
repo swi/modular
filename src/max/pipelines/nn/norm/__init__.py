eb1a71379e0eb161c9270f0bc3420f612a2e547c
abdul dakkak <adakkak@modular.com>
2025-02-14 04:57:27 -0800

    [MAX] Rename lp_norm file to layer_norm, NFC (#55963)
    MODULAR_ORIG_COMMIT_REV_ID: 95172d6e125844e5a847e05aa032ee727fc4f5b6

--- src/max/pipelines/nn/norm/__init__.py
+++ src/max/pipelines/nn/norm/__init__.py
@@ -11,7 +11,7 @@
 # limitations under the License.
 # ===----------------------------------------------------------------------=== #
 
-from .lp_norm import LayerNorm
+from .layer_norm import LayerNorm
 from .rms_norm import DistributedRMSNorm, RMSNorm, RMSNormV2
 
 __all__ = ["LayerNorm", "RMSNorm", "RMSNormV2", "DistributedRMSNorm"]
